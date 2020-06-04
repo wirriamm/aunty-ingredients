8682e16019c87229e97732a131a0a3663a3585b5
Jasmine Dang <ngandth.1992@gmail.com>
2020-06-04 20:47:34 +0800

    validation for payment and order done

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,7 @@ class Order < ApplicationRecord
   # relationship
   belongs_to :user
   belongs_to :payment
+
+  # validation
+  validates :quantity_ordered, presence: true, numericality: {only_integer: true}
 end
--- app/models/payment.rb
+++ app/models/payment.rb
@@ -2,4 +2,8 @@ class Payment < ApplicationRecord
   # relationship
   belongs_to :user
   has_many :orders
+
+  # validation
+  validates :total_price, presence: true, numericality: true
+
 end
