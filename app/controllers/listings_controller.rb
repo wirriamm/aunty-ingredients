60305875c5d5427d7ea5732aa7b7eaf3383a9d95
deb wagon <deb.fsmk@gmail.com>
2020-06-04 21:13:04 +0800

    listings controller completed + validations

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -3,6 +3,42 @@ class ListingsController < ApplicationController
     @listings = Listing.all
   end
 
+  def show
+    @listing = Listing.find(params[:id])
+  end
+
   def new
+    @listing = Listing.new
+  end
+
+  def create
+    @listing = Listing.new(params.require(:listing).permit(:name, :description, :quantity_available, :listing_price_pq))
+    @listing.user = current_user
+    @listing.save
+    if @listing.save
+      redirect_to listing_path(@listing)
+    else
+      render "new"
+    end
+  end
+
+  def edit
+    @listing = Listing.find(params[:id])
+  end
+
+  def update
+    @listing = Listing.find(params[:id])
+    @listing.update(params.require(:listing).permit(:name, :description, :quantity_available, :listing_price_pq))
+    if @listing.update(params.require(:listing).permit(:name, :description, :quantity_available, :listing_price_pq))
+      redirect_to listing_path(@listing)
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @listing = Listing.find(params[:id])
+    @listing.destroy
+    redirect_to listings_path
   end
 end
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,9 @@
 class Listing < ApplicationRecord
   belongs_to :user
   has_one_attached :picture
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :quantity_available, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :listing_price_pq, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
