ca45c379906318e998dc28c0af360b45fc777097
deb wagon <deb.fsmk@gmail.com>
2020-06-04 20:01:52 +0800

    tried to add user seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "cleaning db"
+User.destroy_all
+Listing.destroy_all
 
+puts "seeding users"
+user1 = {
+  email: "user1#@test.com",
+  encrypted_password: "password1"
+}
+user2 = {
+  email: "user2@test.com",
+  encrypted_password: "password2"
+}
 
+[ user2, user2 ].each do |user|
+  User.create!(user)
+end
+puts "users seeded"
+
+# puts "creating listings"
