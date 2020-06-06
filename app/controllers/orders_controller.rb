28b6e81c3d428d0ae8c6b53c3a912b0377f2e8f3
William Goh <wirriamm@gmail.com>
2020-06-06 22:22:01 +0800

    orders#index validates the quantity against available listing when basket is refreshed

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,7 @@
 class OrdersController < ApplicationController
   def index
     @orders = Order.where(user: current_user, completed: false)
+    @orders = @orders.select { |order| validate_quantity(order) }
     @payment = Payment.new
     @total_price = total_price
   end
@@ -11,11 +12,16 @@ class OrdersController < ApplicationController
       user: current_user,
       listing: listing,
 <<<<<<< HEAD
+<<<<<<< HEAD
       order_price_pq: listing.listing_price_pq,
 =======
       order_price_pq: listing.listing_price_pq
 >>>>>>> orders#index only show orders that belongs to user and also orders that have not been transacted
       quantity_ordered: params[:quantity_ordered]
+=======
+      order_price_pq: listing.listing_price_pq,
+      quantity_ordered: params[:order][:quantity_ordered]
+>>>>>>> orders#index validates the quantity against available listing when basket is refreshed
       )
     if @order.quantity_ordered > @order.listing.quantity_available
       redirect_to listing_path(listing), alert: "Only #{@order.listing.quantity_available} of #{@order.listing.name.capitalize} is available. Please change the quantity of your order."
@@ -30,10 +36,10 @@ class OrdersController < ApplicationController
     @order = Order.find(params[:id])
     @order.quantity_ordered = params[:order][:quantity_ordered]
     if @order.quantity_ordered > @order.listing.quantity_available
-      redirect_to orders_path, alert: "Only #{@order.listing.quantity_available} of #{@order.listing.name} is available. Please change the quantity of your order."
+      redirect_to orders_path, alert: "Only #{@order.listing.quantity_available} #{@order.listing.name} available. Please change the quantity of your order."
     else
       if @order.save
-        redirect_to orders_path, notice: "#{@order.quantity_ordered} of #{@order.listing.name} has been added to updated in your basket."
+        redirect_to orders_path, notice: "#{@order.quantity_ordered} #{@order.listing.name} updated in your basket."
       end
     end
   end
@@ -46,6 +52,7 @@ class OrdersController < ApplicationController
 
   private
 
+<<<<<<< HEAD
   def total_price
     sum = 0
     @orders = Order.where(user: current_user, completed: false)
@@ -54,5 +61,9 @@ class OrdersController < ApplicationController
       sum += order_price
     end
     sum
+=======
+  def validate_quantity(order)
+    order.quantity_ordered <= order.listing.quantity_available
+>>>>>>> orders#index validates the quantity against available listing when basket is refreshed
   end
 end
