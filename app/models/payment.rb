b4bf888f38a832fe03c3d4ee9ff5fcb675093fdd
William Goh <59186927+wirriamm@users.noreply.github.com>
2020-06-06 15:15:12 +0800

    Removed option:true
    When creating a payment, orders must be linked to payment, else no point making the payment instance

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,7 +1,7 @@
 class Payment < ApplicationRecord
   # relationship
   belongs_to :user
-  has_many :orders, optional: true
+  has_many :orders
 
   # validation
   validates :total_price, presence: true, numericality: true
