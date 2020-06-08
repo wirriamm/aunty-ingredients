410bf91261ad63fda779fe3c47f71528f148ab91
Jasmine Dang <ngandth.1992@gmail.com>
2020-06-08 22:21:20 +0800

    combine new order with existing order if there is

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,6 @@
 class OrdersController < ApplicationController
+  before_action :set_listing, only: [:create]
+
   def index
     orders = Order.where(user: current_user, completed: false)
     @removed = orders.reject { |order| validate_quantity?(order) }
@@ -8,13 +10,19 @@ class OrdersController < ApplicationController
   end
 
   def create
-    listing = Listing.find(params[:listing_id])
-    @order = Order.new(
-      user: current_user,
-      listing: listing,
-      order_price_pq: listing.listing_price_pq,
-      quantity_ordered: params[:order][:quantity_ordered]
-      )
+    if order_exist_in_cart?
+      @order = find_existing_order
+      @order.quantity_ordered += params[:order][:quantity_ordered].to_i
+      @order.save
+    else
+      @order = Order.new(
+        user: current_user,
+        listing: @listing,
+        order_price_pq: @listing.listing_price_pq,
+        quantity_ordered: params[:order][:quantity_ordered]
+        )
+    end
+
     if validate_quantity?(@order)
       if @order.save
         redirect_to orders_path, notice: "#{@order.quantity_ordered} #{@order.listing.name} added to your basket."
@@ -59,5 +67,19 @@ class OrdersController < ApplicationController
 
   def validate_quantity?(order)
     order.quantity_ordered <= order.listing.quantity_available
+  end
+
+  def set_listing
+    @listing = Listing.find(params[:listing_id])
+  end
+
+  def find_existing_order
+    Order.where(user: current_user,
+                listing: @listing,
+                completed: false).first
+  end
+
+  def order_exist_in_cart?
+    find_existing_order ? true : false
   end
 end
